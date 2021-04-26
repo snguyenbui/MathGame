4e93c1c29d6483a0476078ff3cd6b304cf0327db
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-26 20:41:08 +0000

    implemented full logic

--- main.rb
+++ main.rb
@@ -2,10 +2,35 @@ require "./Game"
 require "./Question"
 require "./Player"
 
-# gets.chomp
-player1 = Player.new(3);
-player2 = Player.new(3);
-question = Question.new();
-question.create()
-puts question.value1
-puts question.value2
+# Set up required variables
+player1 = Player.new(1)
+player2 = Player.new(3)
+question = Question.new()
+game = Game.new()
+game.current_player = player1
+
+while(game.not_over?)
+  question.create()
+  question.ask()
+  response = gets.chomp
+
+  if !question.isAnswer?(response)
+    game.current_player.lives += -1
+  end
+
+  if game.not_over?
+    if game.current_player == player1
+      puts "Player 2's turn"
+      game.current_player = player2
+    else
+      puts "Player 1's turn"
+      game.current_player = player1
+    end
+  else
+    puts "You lose"
+  end
+end
+
+puts "Game over"
+puts "Score"
+puts "Player 1: #{player1.lives} - Player 2: #{player2.lives}"
