7d57e47a596ba2d1e9da51650cd783c82d949ff5
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-26 21:09:00 +0000

    added random operators

--- Question.rb
+++ Question.rb
@@ -3,16 +3,31 @@ class Question
     @value1
     @value2
     @answer
+    @operand
   end
 
   def create
     @value1 = Random.rand(20)
     @value2 = Random.rand(20)
-    @answer = @value1 + @value2
+    test = Random.rand(5)
+
+    if test == 0
+      @operand = :+
+    elsif test == 1
+      @operand = :-
+    elsif test == 2
+      @operand = :*
+    elsif test == 3
+      @operand = :/
+    elsif test == 4
+      @operand = :%
+    end
+    
+    @answer = @value1.public_send(@operand, @value2)
   end
 
   def ask
-    puts "What is #{@value1} + #{@value2}?"
+    puts "What is #{@value1} #{@operand} #{@value2}?"
   end
 
   def isAnswer?(response)
